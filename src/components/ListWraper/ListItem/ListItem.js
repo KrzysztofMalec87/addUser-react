407f057b5aff490566b33af9634438c89f965b94
krzysiek <krzysztof@like.agency>
2019-07-29 21:48:25 +0200

    Change: ListItem.js component, add: prop types

--- src/components/ListWraper/ListItem/ListItem.js
+++ src/components/ListWraper/ListItem/ListItem.js
@@ -1,24 +1,32 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const ListItem = ({id, name, surname, location, age}) => (
-  <tr dataRowToRemove={id}>
-    <td>
+  <tr className="list-wraper__table-row" dataRowToRemove={id}>
+    <td className="list-wraper__table-cell">
       {name}
     </td>
-    <td>
+    <td className="list-wraper__table-cell">
       {surname}
     </td>
-    <td>
+    <td className="list-wraper__table-cell">
       {location}
     </td>
-    <td>
+    <td className="list-wraper__table-cell">
       {age}
     </td>
-    <td className="ListWraper__td ListWraper__td--remove">
-      <button className="btn btn-danger btn-sm" dataRowRemove={id}>X</button>
+    <td className="list-wraper__table-cell list-wraper__table-cell--remove">
+      <button className="btn btn-danger btn-sm ist-wraper__remove-button" dataRowRemove={id}>X</button>
     </td>
   </tr>
  );
+
+
+ ListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
 
 export default ListItem;
