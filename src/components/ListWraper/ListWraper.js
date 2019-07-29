e4101f1a64f9c7370a3aa12db73f6cc4f79048c9
krzysiek <krzysztof@like.agency>
2019-07-29 21:48:54 +0200

    Change: ListWraper.js component

--- src/components/ListWraper/ListWraper.js
+++ src/components/ListWraper/ListWraper.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 
-const ListWrapper = props => (
-  <table className="table table-striped table-bordered ListWraper">
-    <thead className="table-dark">
-      {props.items.header.map(item => (
-        <ListItem key={item.id} {...item} />
-      ))}
-    </thead>
-    <tbody>
-      {props.items.users.map(item => (
-        <ListItem key={item.id} {...item} />
-      ))}
-    </tbody>
-  </table>
-);
+const ListWrapper = ({items}) => {
+  const { header, users } = items;
+
+  return (
+    <table className="table table-striped table-bordered list-wraper">
+      <thead className="table-dark">
+        {header.map(item => {
+          const { id } = item; 
+
+          return (
+            <ListItem key={id} {...item} />
+          )
+         }
+        )}
+      </thead>
+      <tbody>
+      {users.map(item => {
+          const { id } = item; 
+
+          return (
+            <ListItem key={id} {...item} />
+          )
+         }
+        )}
+      </tbody>
+    </table>
+  );
+};
 
 export default ListWrapper;
