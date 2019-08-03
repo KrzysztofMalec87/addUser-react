9943c57ff181e8949aed5ae193505716a4aa4fea
krzysiek <krzysztof@like.agency>
2019-08-03 23:15:23 +0200

    Add function that build new state

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  
+submitForm = event => {
+  event.preventDefault();
 
-  handleSubmitForm = event => {
-    event.preventDefault();
+  const {
+    header,
+    users
+  } = this.props.state;
+  const form = event.target;
+  const formInputName = form.name.value;
+  const formInputSurname = form.surname.value;
+  const formInputLocation = form.location.value;
+  const formInputAge = form.age.value;
+  const randomId = Math.floor(Math.random() * 10000);
 
-    event.target.reset();  
-  }
+  const newState = {
+    header: [...header],
+    users: [
+      ...users,
+      {
+        id: `${randomId}`,
+        name: `${formInputName}`,
+        surname: `${formInputSurname}`,
+        location: `${formInputLocation}`,
+        age: `${formInputAge}`
+      }
+    ]
+  };
+
+
+  this.props.changeState(newState);
+
+  form.reset();
+}
+
 
   render(){
     return (
-      <form autoComplete="off" onSubmit={this.handleSubmitForm}>
+      <form autoComplete="off" onSubmit={this.submitForm}>
         <div className="row">
           <div className="col">
-            <input className="form-control" type="text" />
+            <input className="form-control" minLength="3" name="name" required type="text" />
           </div>
           <div className="col">
-            <input className="form-control" type="text" />
+            <input className="form-control" minLength="3" name="surname" required type="text" />
           </div>
           <div className="col">
-            <input className="form-control" type="text" />
+            <input className="form-control" minLength="3" name="location" required type="text" />
           </div>
           <div className="col">
-            <input className="form-control" type="text" />
+            <input className="form-control" name="age" required type="number" />
           </div>
           <div className="col">
             <button className="btn btn-info w-100">Add</button>
