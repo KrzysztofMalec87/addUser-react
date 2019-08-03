6e1dac318eed4bcc18cb8e8fb46c3e438d57b379
krzysiek <krzysztof@like.agency>
2019-08-03 23:16:27 +0200

    Change list item component structure

--- src/components/ListWraper/ListItem/ListItem.js
+++ src/components/ListWraper/ListItem/ListItem.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const ListItem = ({id, name, surname, location, age}) => (
-  <tr className="list-wraper__table-row" dataRowToRemove={id}>
-    <td className="list-wraper__table-cell">
-      {name}
-    </td>
-    <td className="list-wraper__table-cell">
-      {surname}
-    </td>
-    <td className="list-wraper__table-cell">
-      {location}
-    </td>
-    <td className="list-wraper__table-cell">
-      {age}
-    </td>
-    <td className="list-wraper__table-cell list-wraper__table-cell--remove">
-      <button className="btn btn-danger btn-sm list-wraper__remove-button" dataRowRemove={id}>X</button>
-    </td>
-  </tr>
- );
+import React, { Component } from 'react';
 
 
- ListItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  surname: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-};
+class ListItem extends Component {
+
+  findindexKey = ( array, id ) => array.findIndex( object => object.id === id );
+  
+  removeRow = () => {
+    const {
+      id,
+      state
+    } = this.props;
+
+    delete state.users[ this.findindexKey(state.users, id) ];
+
+    state.users = state.users.filter(function(){return true;});
+
+    this.props.changeState(state);
+  }
+  
+  render(){
+    const {
+      name,
+      surname,
+      location,
+      age
+    } = this.props;
+    
+    
+    return (
+      <tr className="list-wraper__table-row">
+        <td className="list-wraper__table-cell">
+          {name}
+        </td>
+        <td className="list-wraper__table-cell">
+          {surname}
+        </td>
+        <td className="list-wraper__table-cell">
+          {location}
+        </td>
+        <td className="list-wraper__table-cell">
+          {age}
+        </td>
+        <td className="list-wraper__table-cell list-wraper__table-cell--remove">
+          <button className="btn btn-danger btn-sm list-wraper__remove-button" onClick={this.removeRow}>X</button>
+        </td>
+      </tr>
+    );
+  }
+}
+
 
 export default ListItem;
